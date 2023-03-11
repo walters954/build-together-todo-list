8d365919bb37b32b189f5cfe68afd5140a4e6bfd
Dewayne Scott <dewaynescott@Dewaynes-Mini.lan>
2023-03-11 11:26:06 -0500

    Updates to project: html and add an app

--- force-app/main/default/lwc/todoList/todoList.js
+++ force-app/main/default/lwc/todoList/todoList.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class TodoList extends LightningElement {}
+export default class TodoList extends LightningElement {
+
+    // priority levels are 1, 2, or 3
+    priority;
+
+    priority = 1;
+
+    // Add new todo list item
+    handleAddItem(even){
+
+    }
+
+    // Delte todo list item
+    handleDeleteItem(even){
+
+    }
+}
