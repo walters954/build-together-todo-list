e4ee260636c315189edc6bfe9cb180c0c120d063
Warren Walters <warren.salesforce@gmail.com>
2023-03-11 13:21:34 -0600

    Basic Reactive LWC.
    Co-authored-by: dewscott7 <dewscott7@users.noreply.github.com>
    Co-authored-by: Aiswarya Saripella <AiswaryaSaripella@users.noreply.github.com>

--- force-app/main/default/lwc/todoList/todoList.js
+++ force-app/main/default/lwc/todoList/todoList.js
@@ -1,3 +1,26 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class TodoList extends LightningElement {}
+export default class TodoList extends LightningElement {
+
+    tasks = [0,1,2,3,4,5,6];
+    
+    handleClick(event) {
+        console.log(event.target.label);
+        let newItem = Math.floor(Math.random() * 100);
+        
+        let newTasks;
+        //spread operator
+        newTasks = [...this.tasks,newItem];
+        //spread operator
+        //deep clone
+        newTasks = JSON.parse(JSON.stringify(this.tasks)); 
+        newTasks.push(newItem);
+        //deep clone
+        //concat 
+        newTasks = this.tasks.concat(newItem);
+        //concat 
+
+        this.tasks = newTasks;
+        console.log(this.tasks);
+    }
+}
