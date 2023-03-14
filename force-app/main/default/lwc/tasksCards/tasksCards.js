f174a6e80a0c27a066fe34c6d46a3877edee4194
CDNHOPHEAD <adam.k.dalton@gmail.com>
2023-03-14 20:26:23 -0300

    Changes to be committed: 	modified:   force-app/main/default/lwc/tasksCards/tasksCards.html 	modified:   force-app/main/default/lwc/tasksCards/tasksCards.js

--- force-app/main/default/lwc/tasksCards/tasksCards.js
+++ force-app/main/default/lwc/tasksCards/tasksCards.js
@@ -1,40 +1,25 @@
-import { LightningElement,api,track } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
 export default class TasksCards extends LightningElement {
-
     @track newTaskDetails;
-    @track tasks = [
-        {
-            id:'1',
-            taskDescription:'task 1',
-            category:'personal',
-            deadLine:new Date("2023-03-25").toLocaleDateString("en-US")
-        },
-        {
-            id:'2',
-            taskDescription:'task 2',
-            category:'work',
-            deadLine:new Date('2023-03-17').toLocaleDateString("en-US")
-        },
-        {
-            id:'3',
-            taskDescription:'task 3',
-            category:'health',
-            deadLine:new Date('2023-03-20').toLocaleDateString("en-US")
-        }
-    ];
-    @track taskLength = this.tasks.length;
+    @track tasks = [];
 
-    @api getNewTaskDetails(newTask){
-        //console.log('child component: '+JSON.stringify(newTask));
+    @api getNewTaskDetails(newTask) {
         this.newTaskDetails = {
-            id:this.taskLength,
+            id: new Date().getTime().toString(),
             taskDescription: newTask.taskDescription,
-            category:newTask.category,
-            deadLine:newTask.taskDeadline
-        }
-        //console.log(JSON.stringify(this.newTaskDetails));
+            category: newTask.category,
+            deadLine: newTask.taskDeadline,
+        };
         this.tasks.push(this.newTaskDetails);
-        //console.log(JSON.stringify(this.tasks));
+    }
+
+    handleDeleteTask(event) {
+        const taskId = event.target.dataset.taskId;
+        this.deleteTask(taskId);
+    }
+
+    deleteTask(taskId) {
+        this.tasks = this.tasks.filter(task => task.id !== taskId);
     }
 }
