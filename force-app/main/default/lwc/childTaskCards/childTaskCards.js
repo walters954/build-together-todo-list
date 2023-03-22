e6566387357a0d3faba741981b9dafb2e4a5b791
aiswaryasaripella <aiswaryasaripella@gmail.com>
2023-03-22 13:59:56 -0700

    Edit Functionality on task object

--- force-app/main/default/lwc/childTaskCards/childTaskCards.js
+++ force-app/main/default/lwc/childTaskCards/childTaskCards.js
@@ -1,13 +1,23 @@
-import { api,LightningElement } from 'lwc';
+import { api,track,LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import deleteTask from '@salesforce/apex/TodoAppController.deleteTask';
 import getTaskDetails from '@salesforce/apex/TodoAppController.getTaskDetails';
+import getStatusPicklistValues from '@salesforce/apex/TodoAppController.getStatusPicklistValues';
+import getPriorityPicklistValues from '@salesforce/apex/TodoAppController.getPriorityPicklistValues';
+import updateTask from '@salesforce/apex/TodoAppController.updateTask';
 
 export default class ChildTaskCards extends LightningElement {
     @api task;
+    @track subject;
+    @track status;
+    @track priority;
+    @track dueDate;
+    @track statusOptions;
+    @track priorityOptions;
+    @track editEnabled = false;
 
     deleteTask(event){
-        const Id = event.target.dataset.taskId;
+         const Id = event.target.dataset.taskId;
         console.log(Id);
 
         //delete the task record
@@ -34,10 +44,70 @@ export default class ChildTaskCards extends LightningElement {
     }
 
     editTask(event){
+        this.editEnabled = true;
         const Id = event.target.dataset.taskId;
+        getStatusPicklistValues().then(result => 
+            {
+                let options = [];
+                if(result){
+                    result.forEach(r => {
+                        options.push({label:r,value:r});
+                    });
+                }
+                this.statusOptions = options;
+            }).catch(error =>{console.log(error);});
 
-    getTaskDetails({taskId:Id}).then(result =>{
-        console.log(result);}).catch(error => {console.log(error);});
+        getPriorityPicklistValues().then(result => 
+                {
+                    let options = [];
+                    if(result){
+                        result.forEach(r => {
+                            options.push({label:r,value:r});
+                        });
+                    }
+                    this.priorityOptions = options;
+                }).catch(error =>{console.log(error);});
 
+        getTaskDetails({taskId:Id}).then(result =>{
+            this.subject = result.Subject;
+            this.dueDate = result.ActivityDate;
+            this.priority = result.Priority;
+            this.status = result.Status;
+        }).catch(error => {console.log(error);});
+    }
+
+    handleInputChange(event){
+        const field = event.target.name;
+        const value = event.target.value;
+        this[field]=value;
+    }
+
+    updateTask(event){
+        const Id = event.target.dataset.taskId;
+        updateTask({taskId:Id,subject:this.subject,status:this.status,priority:this.priority}).then(result => {
+            const event = new ShowToastEvent({
+                title:'Update Success',
+                variant:'success',
+                message:'Record Deleted Successfully'
+            });
+            this.dispatchEvent(event);
+            this.editEnabled = false;
+            if(!this.editEnabled){
+                getTaskDetails({taskId: Id}).then(result =>{
+                    this.task = result;
+                })
+            }
+        }).catch(error => {
+            const event = new ShowToastEvent({
+                title:'Updated failed',
+                variant:'error',
+                message:'Record Update Failed '+error.body.message
+            });
+            this.dispatchEvent(event);
+            console.log(error);})
+    }
+
+    cancelTask(){
+        this.editEnabled = false;
     }
 }
