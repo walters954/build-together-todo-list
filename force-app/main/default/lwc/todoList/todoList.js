06499a0bb495773c409258cc462b2ad29503dbbc
Warren Walters <warren.salesforce@gmail.com>
2023-03-23 20:09:37 -0500

    Week 4 working together
    Co-authored-by: gnonwegnon adoukonou <totololo@users.noreply.github.com>
    Co-authored-by: dewscott7 <dewscott7@users.noreply.github.com>

--- force-app/main/default/lwc/todoList/todoList.js
+++ force-app/main/default/lwc/todoList/todoList.js
@@ -1,34 +1,57 @@
-import { LightningElement, track } from 'lwc';
-import TestChild from 'c/testChild';
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+// import TestChild from 'c/testChild';
 
 export default class TodoList extends LightningElement  {
     showModal
 
     tasks = [0,1,2,3,4,5,6];
+
+    get taskLength(){
+        return this.tasks.length;
+    }
     
     handleClick(event) {
-        TestChild.open().then((result) => {
-            console.log(result);
-        });
+        // TestChild.open().then((result) => {
+        //     console.log(result);
+        // });
         console.log(event.target.label);
-        let newItem = Math.floor(Math.random() * 100);
+        let newItem = Math.floor(Math.random() * 1000);
         
-        let newTasks;
         //spread operator
-        newTasks = [...this.tasks,newItem];
-        //spread operator
-        //deep clone
-        newTasks = JSON.parse(JSON.stringify(this.tasks)); 
-        newTasks.push(newItem);
-        //deep clone
-        //concat 
-        newTasks = this.tasks.concat(newItem);
-        //concat 
-
+        let newTasks = [...this.tasks,newItem];
         this.tasks = newTasks;
         console.log(this.tasks);
+        this.successToast();
     }
+    
     handleCancel(){
         this.showModal=false;
+    }
+
+    deleteAll(){
+        console.log('delete all');
+        this.tasks = [];
+        this.deleteToast();
+    }
+
+    deleteToast() {
+        const event = new ShowToastEvent({
+            title: 'Success',
+            variant: 'error',
+            message:
+                'All tasks successfully deleted.',
+        });
+        this.dispatchEvent(event);
+    }
+
+    successToast() {
+        const event = new ShowToastEvent({
+            title: 'Line added successfully!',
+            variant: 'success',
+            message:
+                'You are the best!',
+        });
+        this.dispatchEvent(event);
     }
 }
